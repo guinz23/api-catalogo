140c92b474d2a445ad2765db3116631820c960f2
gabriel <juangabriellopez30@gmail.com>
2019-12-03 13:40:00 -0600

    second commit

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('http://api-catalogo.herokuapp.com/api'),
 
     'asset_url' => env('ASSET_URL', null),
 
