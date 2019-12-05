b75599191bcaf0de069163c7d5f5e44cec39162e
gabriel <juangabriellopez30@gmail.com>
2019-12-04 20:26:10 -0600

    first commit

---
+++ app/Http/Controllers/CustomArticlesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\CustomArticles;
+use Illuminate\Http\Request;
+
+class CustomArticlesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\CustomArticles  $customArticles
+     * @return \Illuminate\Http\Response
+     */
+    public function show(CustomArticles $customArticles)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\CustomArticles  $customArticles
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(CustomArticles $customArticles)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\CustomArticles  $customArticles
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, CustomArticles $customArticles)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\CustomArticles  $customArticles
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(CustomArticles $customArticles)
+    {
+        //
+    }
+}
